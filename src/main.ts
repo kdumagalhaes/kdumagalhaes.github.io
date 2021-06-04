d25c99f8b6253b62e0b3c0531b866e776f3eb472
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-06-03 22:39:30 -0300

    finished basic configuration with basic style variables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/css/_reset.scss'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).mount('#app')
