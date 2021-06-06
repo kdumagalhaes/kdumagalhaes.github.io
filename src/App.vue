2a39513b6f778bf6983f12aba6a27de66028474e
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-06-06 15:22:25 -0300

    Finished Profile component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-    <h1>salut, france</h1>
+    <Profile
+        picSrc="../assets/img/profile-pic.png"
+        text="Graduated in Advertising, Software Development student currently
+                working as Lead Product Specialist... and sometimes a musician
+                :)"
+    />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Profile } from '@/components/index'
+
+export default defineComponent({
+    components: {
+        Profile,
+    },
+})
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/Profile/Profile.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="profile">
+        <img
+            src="@/assets/img/profile-pic.png"
+            alt="carlos pereira profile pic"
+        />
+        <div class="profile__divider">
+            <h1>hi, I'm <strong>Kadu!</strong></h1>
+            <p>
+                {{ text }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+    },
+})
+</script>
+
+<style scoped lang="scss">
+.profile {
+    display: flex;
+    align-items: center;
+
+    img {
+        margin-right: 15px;
+    }
+
+    h1 {
+        font-size: 1.5rem;
+        margin-bottom: 10px;
+        font-weight: $light;
+    }
+
+    p {
+        width: 100%;
+        max-width: 220px;
+        font-size: 0.8rem;
+        line-height: 18px;
+    }
+}
+</style>
